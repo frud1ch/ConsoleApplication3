cd9f7843889a5e9c17be23687e890206953370b6
root <root@MB-C-3-9-PC12>
2023-01-12 13:29:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+template<typename A> class Human {
+public:
+    int health;
+    A name;
+};
+using namespace std;
+
+int main()
+{
+    Human<int> One;
+    return 0;
+}

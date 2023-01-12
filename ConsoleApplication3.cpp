709208b3ae95a23076eff1610bb9bb9277ca3407
bppk <root@MB-C-3-9-PC12>
2023-01-12 13:57:46 +0300

    1

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -3,15 +3,25 @@
 
 #include <iostream>
 
-template<typename A> class Human {
+template<typename A,typename B> class Human {
 public:
-    int health;
-    A name;
+    B health;
+    A name; 
+    A getter(int) {
+        return name;
+    }
+    B getter(char) {
+        return health;
+    }
 };
 using namespace std;
 
 int main()
 {
-    Human<int> One;
+    Human<string, int> One;
+    Human<char, double> Two;
+    One.name = "Igor";
+    Two.name = 'R';
+    std::cout << One.getter(4);
     return 0;
 }
